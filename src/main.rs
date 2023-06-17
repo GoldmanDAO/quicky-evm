5f41dbc267d87a6d95214a692c957a990fc289d5
Carlos Juarez <carlosgj94@gmail.com>
2023-06-18 01:14:28 +0200

    🔥: adding the runtime to main execution thread

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,13 @@ struct OpcodeInfo {
     opcode_word: Option<Vec<u8>>,
 }
 
+struct ExecutionRuntime {
+    stack: Vec<Vec<u8>>,
+    bytecode: String,
+    opcodes: Vec<op_codes::Opcode>,
+    runtime_position: u64,
+}
+
 fn stack_to_string(stack: &Vec<Vec<u8>>) -> String {
     stack
         .iter()
@@ -46,15 +53,24 @@ impl fmt::Debug for OpcodeInfo {
 
 fn main() {
     let bytecode = "600160020102596018596021596101f4";
+    // let mut parsed: Vec<op_codes::Opcode>;
+
+    let mut runtime = ExecutionRuntime {
+        stack: Vec::new(),
+        bytecode: bytecode.to_string(),
+        opcodes: Vec::new(),
+        runtime_position: 0,
+    };
 
     // let mut stack = Stack::new();
-    let mut stack: Vec<Vec<u8>> = Vec::new();
+    // let mut stack: Vec<Vec<u8>> = Vec::new();
 
-    match op_codes::parse_bytecode(bytecode) {
+    match op_codes::parse_bytecode(runtime.bytecode.as_str()) {
         Ok(parsed) => {
+            runtime.opcodes = parsed;
             let mut byte_position = 0;
 
-            for opcode in parsed {
+            for opcode in runtime.opcodes {
                 let opcode_info = OpcodeInfo {
                     position: byte_position,
                     opcode_name: opcode.name.clone(),
@@ -63,13 +79,15 @@ fn main() {
 
                 println!("{:?}", opcode_info);
 
-                opcode.operation.execute(&mut stack, opcode.word);
-                println!("Stack: {}", stack_to_string(&stack));
+                opcode.operation.execute(&mut runtime.stack, opcode.word);
+                println!("Stack: {}", stack_to_string(&runtime.stack));
 
                 byte_position += 1 + opcode_info
                     .opcode_word
                     .as_ref()
                     .map_or(0, |word| word.len());
+
+                runtime.runtime_position += 1;
             }
         }
         Err(error) => {
