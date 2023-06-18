6f422efea952b22f0fb97fe133aa60607df2a639
Mario S.O <gh_mario.proclaim@slmail.me>
2023-06-18 10:18:47 -0400

    🔧: Fixed sub operation

--- src/op_codes/operations/sub_operation.rs
+++ src/op_codes/operations/sub_operation.rs
@@ -12,7 +12,14 @@ impl CodeOperation for SubOperation {
         let num1 = UBig::from_str_radix(&hex_str1, 16).unwrap();
         let num2 = UBig::from_str_radix(&hex_str2, 16).unwrap();
 
-        let result = num1 - num2;
-        stack.push(result.to_be_bytes());
+        if num1 > num2 {
+            let result = num1 - num2;
+            stack.push(result.to_be_bytes());
+        } else if num2 > num1 {
+            let result = num2 - num1;
+            stack.push(result.to_be_bytes());
+        } else {
+            stack.push(0_u8.to_be_bytes().to_vec());
+        }
     }
 }
