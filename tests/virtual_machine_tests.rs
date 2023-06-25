dd6cecea4bb5b142700990b6d881b296abe3ae5b
Carlos Juarez <carlosgj94@gmail.com>
2023-06-25 22:21:22 +0200

    ✨: test for the jump op code

--- tests/virtual_machine_tests.rs
+++ tests/virtual_machine_tests.rs
@@ -44,3 +44,25 @@ fn stops_running_with_stop() {
     assert_eq!(runtime.stack[0][0], 0x1);
     assert_eq!(runtime.runtime_position, 4);
 }
+
+#[test]
+fn jump_integration() {
+    let bytecode = "60085600000000006001";
+
+    let mut runtime = ExecutionRuntime {
+        stack: Vec::new(),
+        bytecode: bytecode.to_string(),
+        opcodes: Vec::new(),
+        runtime_position: 0,
+        pc: 0,
+        chain_settings: ChainSettings::new(),
+        block_info: BlockInfo::from_zero(),
+    };
+
+    runtime.run();
+
+    assert_eq!(runtime.bytecode, bytecode);
+    assert_eq!(runtime.stack.len(), 1);
+    assert_eq!(runtime.stack[0][0], 0x1);
+    assert_eq!(runtime.runtime_position, 8);
+}
